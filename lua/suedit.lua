3685164bc58c0bee084244e3bb1b133cb619bb36
Grafcube <grafcube@disroot.org>
2025-08-14 19:42:20 +0530

    Remove code

--- lua/suedit.lua
+++ lua/suedit.lua
@@ -6,27 +6,10 @@ function M.setup(opts)
 	opts = opts or {}
 	M.cmd = opts.cmd or M.cmd
 
-	vim.api.nvim_create_user_command("SuWrite", function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local filepath = vim.api.nvim_buf_get_name(bufnr)
-		M.privileged_write(filepath)
-	end, {})
-
 	vim.api.nvim_create_autocmd("BufWritePre", {
 		pattern = "*",
 		callback = M.save_file,
 	})
-end
-
-local function can_write(file)
-	while file ~= "/" do
-		if vim.loop.fs_stat(file) ~= nil then
-			return vim.loop.fs_access(file, "W")
-		else
-			file = vim.fn.fnamemodify(file, ":p:h")
-		end
-	end
-	return false
 end
 
 function M.save_file()
@@ -34,43 +17,4 @@ function M.save_file()
 		"Your version of `suedit.nvim` will no longer receive updates. Please change your plugin config to use `<https://codeberg.org/grafcube/suedit.nvim>`",
 		vim.log.levels.ERROR
 	)
-
-	local bufnr = vim.api.nvim_get_current_buf()
-	local filepath = vim.api.nvim_buf_get_name(bufnr)
-
-	if can_write(filepath) then
-		vim.api.nvim_command("write")
-	else
-		M.privileged_write(filepath)
-	end
 end
-
-function M.privileged_write(filepath)
-	local tempname = vim.fn.tempname()
-	vim.api.nvim_command("write! " .. tempname)
-
-	local cmd = string.format("%s cp %s %s", M.cmd, tempname, filepath)
-	M.run_terminal(cmd, tempname)
-end
-
-function M.run_terminal(cmd, tempname)
-	local Terminal = require("toggleterm.terminal").Terminal
-	local term = Terminal:new({
-		cmd = cmd,
-		direction = "float",
-		-- function to run on opening the terminal
-		on_open = function(term)
-			vim.cmd("startinsert!")
-			vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-		end,
-		-- function to run on closing the terminal
-		on_close = function(term)
-			vim.cmd("startinsert!")
-			vim.fn.delete(tempname)
-		end,
-	})
-
-	term:toggle()
-end
-
-return M
